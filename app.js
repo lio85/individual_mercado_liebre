b5f88ff5079add1471210beca5ed57b10147bb6d
lio85 <lionelprats2021@gmail.com>
2021-06-08 21:43:24 -0300

    listo para subir a heroku

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ let path=require('path');
 
 app.use(express.static(path.resolve(__dirname,'public')));
 
-app.listen(3000,()=> console.log('El servidor 3000 está corriendo'));
+// app.listen(3000,()=> console.log('El servidor 3000 está corriendo'));
+
+app.listen(process.env.PORT || 3000, function() {
+     console.log('Servidor corriendo en el puerto 3000');
+})
 
 app.get('/',function(req,res){  
     res.sendFile(path.join(__dirname,'./views/home.html'));
